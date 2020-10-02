fa41502dbc3a851f4b90e0a53ce3c0f9cd3f4fa3
Sanchit Hajela <64305301+xanxit@users.noreply.github.com>
2020-10-02 16:52:29 +0530

    Create array shifting.cpp (#9)

---
+++ array shifting.cpp
@@ -0,0 +1,48 @@
+
+               //LEFT SHIFTING OF AN ARRAY
+
+
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,temp=0;
+    int a[]={1,3,5,8,4,2,5,1,9};
+    temp=a[0];
+    for(i=0;i<9;i++)
+        a[i]=a[i+1];
+    
+    a[8]=temp;    
+    
+    for(i=0;i<9;i++)
+    cout<<a[i]<<" ";
+    
+    return 0;
+}*/
+
+
+
+
+
+                           // RIGHT SHIFTING OF AN ARRAY
+
+
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int i,temp=0;
+    int a[]={1,3,5,8,4,2,5,1,9};
+    temp=a[8];
+     for(i=8;i>=0;i--)
+        a[i]=a[i-1];
+    
+    a[0]=temp;    
+    
+    for(i=0;i<9;i++)
+    cout<<a[i]<<" ";
+    
+    return 0;
+}*/
+    
